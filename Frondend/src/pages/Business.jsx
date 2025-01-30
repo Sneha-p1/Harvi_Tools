329f617a77038c7b673597dd44638cf166d2138e
Sneha-p1 <snehaparambadan@gmail.com>
2025-01-30 10:02:43 +0530

    new

--- Frondend/src/pages/Business.jsx
+++ Frondend/src/pages/Business.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaBuilding, FaCompass, FaCheckCircle } from "react-icons/fa";
-import harviToolsImg from "../assets/images/Manufacturing-ANIM.gif";
+import harviToolsImg from "../assets/images/Molding.gif";
 import PlasticMould1 from "../assets/images/PlasticMould1.jpg";
 import PlasticMould2 from "../assets/images/PlasticMould2.jpg";
 import PressTool1 from "../assets/images/PressTool1.jpeg";
@@ -31,7 +31,7 @@ const Business = () => {
           {/* Left Section */}
           <div>
             <h2 className="text-3xl font-semibold mb-4 flex items-center">
-              <FaCompass className="mr-3 text-yellow-400" /> Our Journey
+              <FaCompass className="mr-3 text-yellow-500" /> Our Journey
             </h2>
             <p className="mb-6">
               Established in September 2019, <strong>HARVI TOOLS</strong> is
@@ -44,21 +44,21 @@ const Business = () => {
             <h3 className="text-2xl font-semibold mt-8">Our Mission</h3>
             <ul className="space-y-4 mt-4">
               <li className="flex items-start">
-                <FaCheckCircle className="mr-3 mt-1 text-yellow-400" />
+                <FaCheckCircle className="mr-3 mt-1 text-yellow-500" />
                 <div>
                   <strong>Highest Quality:</strong> Delivering top-notch
                   products that meet industry standards.
                 </div>
               </li>
               <li className="flex items-start">
-                <FaCheckCircle className="mr-3 mt-1 text-yellow-400" />
+                <FaCheckCircle className="mr-3 mt-1 text-yellow-500" />
                 <div>
                   <strong>Competitive Price:</strong> Providing value without
                   compromising quality.
                 </div>
               </li>
               <li className="flex items-start">
-                <FaCheckCircle className="mr-3 mt-1 text-yellow-400" />
+                <FaCheckCircle className="mr-3 mt-1 text-yellow-500" />
                 <div>
                   <strong>Shortest Lead Time:</strong> Ensuring timely delivery
                   to meet customer demands.
