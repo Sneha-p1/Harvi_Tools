9fbbf7c32bd093d9c33a12caf03d8b9ec18ee832
Sneha P <107140875+Sneha-p1@users.noreply.github.com>
2025-03-03 15:38:28 +0530

    Update vite.config.js

--- Frondend/vite.config.js
+++ Frondend/vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Load environment variables manually
+const API_URL = process.env.VITE_API_URL || 'https://harvitools.onrender.com';
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_URL, // Correct way to access the environment variable
+        target: API_URL, // Use the loaded variable
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
